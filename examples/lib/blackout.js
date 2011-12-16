ee96e3fd1c04ad494e8988d903bf06ccaf3dc1cf
unknown <Richard.Priddy@WSRichardP02.amtdata.local>
2011-12-16 15:19:17 +0000

    Have implemented a 'splitter' this allows the div to the left and right of it to be resizable across the X axis.

--- examples/lib/blackout.js
+++ examples/lib/blackout.js
@@ -2511,4 +2511,56 @@
 
   bo.ui.Tree = TreeViewModel;
 
+  ko.bindingHandlers.splitter = {
+    init: function(element, valueAccessor) {
+      var $left, $parent, $right, $splitter, leftMaxWidth, leftMinWidth, leftXBorderWidth, parentLeftBorder, parentOffset, parentWidth, rightMaxWidth, rightMinWidth, rightXBorderWidth, sliderLeftWall, sliderRightWall, splitterOuterWidth, splitterPosition;
+      $splitter = jQuery(element);
+      $parent = $splitter.parent();
+      $left = $splitter.prev();
+      $right = $splitter.next();
+      if ($left.length === 0 || $right.length === 0) return;
+      $splitter.addClass('splitter');
+      parentWidth = $parent.width();
+      splitterOuterWidth = $splitter.outerWidth();
+      rightXBorderWidth = $right.outerWidth() - $right.width();
+      leftXBorderWidth = $left.outerWidth() - $left.width();
+      leftMinWidth = parseInt($left.css('min-width'), 10);
+      rightMinWidth = parseInt($right.css('min-width'), 10);
+      leftMaxWidth = $parent.width() - (rightMinWidth + rightXBorderWidth + leftXBorderWidth + splitterOuterWidth);
+      rightMaxWidth = $parent.width() - (leftMinWidth + leftXBorderWidth + rightXBorderWidth + splitterOuterWidth);
+      $left.css({
+        left: 0,
+        top: 0,
+        bottom: 0,
+        position: 'absolute'
+      });
+      $right.css({
+        right: 0,
+        top: 0,
+        bottom: 0,
+        position: 'absolute'
+      });
+      splitterPosition = ko.observable($left.outerWidth());
+      ko.computed(function() {
+        var desiredLeftWidth, desiredRightWidth;
+        desiredLeftWidth = splitterPosition() - leftXBorderWidth;
+        desiredRightWidth = parentWidth - splitterPosition() - splitterOuterWidth - rightXBorderWidth;
+        $left.css('width', Math.min(leftMaxWidth, Math.max(leftMinWidth, desiredLeftWidth)));
+        $right.css('width', Math.min(rightMaxWidth, Math.max(rightMinWidth, desiredRightWidth)));
+        return $splitter.css('left', Math.min(leftMaxWidth + leftXBorderWidth, Math.max(leftMinWidth + leftXBorderWidth, splitterPosition())));
+      });
+      parentLeftBorder = parseInt($parent.css('border-left-width'), 10);
+      parentOffset = $parent.offset();
+      sliderLeftWall = parentOffset.left + leftMinWidth + leftXBorderWidth + parentLeftBorder;
+      sliderRightWall = parentOffset.left + parentWidth - rightXBorderWidth - parentLeftBorder - rightMinWidth - splitterOuterWidth;
+      return $splitter.draggable({
+        axis: "x",
+        containment: [sliderLeftWall, 0, sliderRightWall, 0],
+        drag: function(event, ui) {
+          return splitterPosition(ui.position.left);
+        }
+      });
+    }
+  };
+
 }).call(this);
